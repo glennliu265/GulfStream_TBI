b6433dde6e19af7f553b87fcdd10ebb8cc0f2f5e
Glenn <glenn.y.liu@gmail.com>
2023-08-03 18:13:53 +0200

    Modified to make more general, work with test .25deg era
    - Modified functions to account/allow for specifying the latitude and longitude names
    - Currently works with 0,25deg ERA5 data, might not work with 1deg ERA5 data. need to go back and fix this.

--- Scrap/gradient_calculation_scrap.py
+++ Scrap/gradient_calculation_scrap.py
@@ -19,7 +19,8 @@ from haversine import Unit
 #%% Set Paths
 
 path = "/Users/gliu/Dropbox (MIT)/Glenn Liu’s files/Home/Work_Portable/ICTP/Project/Data/"
-ncname  = "era5_sst_1940_2022_1deg.nc"
+#ncname  = "era5_sst_1940_2022_1deg.nc"
+ncname = "ERA5_sst_test.nc"
 
 
 figpath = "/Users/gliu/Dropbox (MIT)/Glenn Liu’s files/Home/Work_Portable/ICTP/Project/Figures/"
@@ -27,7 +28,8 @@ figpath = "/Users/gliu/Dropbox (MIT)/Glenn Liu’s files/Home/Work_Portable/ICTP
 #%% Functions
 
 
-def get_gs_coords_alltime(da,n_roll,return_grad=True):
+def get_gs_coords_alltime(da,n_roll,return_grad=True,latname='lat',lonname='lon',
+                          latslice=[20,50],lonslice=[360-75,360-50]):
     
     '''
     INPUTS:
@@ -40,21 +42,33 @@ def get_gs_coords_alltime(da,n_roll,return_grad=True):
     gs_coords: a dataarray containing lat/lon of maximum sst meridional gradient (dimensions nx * 2)
     
     '''
+    # Flip Latitude to go from -90 to 90
+    if (da[latname][1] - da[latname][0]) < 0:
+        print("Flipping Latitude to go from South to North")
+        da = da.isel(**{latname:slice(None,None,-1)})
     
     # Compute Gradient
     da      = da.sst
-    da_grad = get_total_gradient(da,n_roll)
+    da_grad = get_total_gradient(da,n_roll,latname=latname,lonname=lonname)
     
     # Subset region, find max latitude for each longitude
-    da_grad  = da_grad.sel(lat = slice(20,50), lon = slice(360-75,360-50))
-    lats_max = da_grad.argmax(dim='lat')
+    da_grad  = da_grad.sel(**{latname : slice(latslice[0],latslice[1]),lonname : slice(lonslice[0],lonslice[1])})
+    
+    # Remove edge values (a dumb fix :(...)
+    mask     = np.ones(da_grad.shape) 
+    mask[:,[0,-1],:] = 0
+    mask[:,:,[0,-1]] = 0
+    da_grad = da_grad * mask
+    
+    lats_max = da_grad.argmax(dim=latname,skipna=True)
+    
     
     # Retrieve max gradient at each longitude
-    ntime,nlon = lats_max.values.shape
+    ntime,nlon  = lats_max.values.shape
     max_gradient = np.zeros((ntime,nlon)) * np.nan
     for t in range(ntime):
         lat_indices_t = lats_max.isel(time=t)
-        grad_along_gs = da_grad.isel(time=t,lat=lat_indices_t)
+        grad_along_gs = da_grad.isel(**{'time':t,latname:lat_indices_t})
         max_gradient[t,:] = grad_along_gs
     if return_grad:
         return lats_max,max_gradient,da_grad
@@ -70,10 +84,10 @@ def get_gs_coords_alltime(da,n_roll,return_grad=True):
     return lats_max
 
 
-def get_total_gradient(da, n_roll):
+def get_total_gradient(da, n_roll,latname='lat',lonname="lon"):
     
     # Get Longitude
-    lats = da.lat
+    lats  = da[latname]
     lats0 = np.squeeze(np.dstack((lats,np.zeros(len(lats)))))
     lats1 = np.squeeze(np.dstack((lats,np.ones(len(lats)))))
     
@@ -82,14 +96,14 @@ def get_total_gradient(da, n_roll):
     ydist = 111*(lats[1]-lats[0]) # take distance between latitudes as 111 km
     
     # Compute Meridional
-    y1    = da.roll({'lat':n_roll},roll_coords=False)
-    y2    = da.roll({'lat':-1*n_roll},roll_coords=False)
+    y1    = da.roll({latname:n_roll},roll_coords=False)
+    y2    = da.roll({latname:-1*n_roll},roll_coords=False)
     ygrad = (y1 - y2)/(2 * n_roll * ydist)
     #ygrad1 = (da.roll({'lat':n_roll}) - da.roll({'lat':-1*n_roll}))/(2 * n_roll * ydist)
     
     # Compute Zonal
-    x1  = da.roll({'lon':n_roll},roll_coords=False)
-    x2  = da.roll({'lon':-1*n_roll},roll_coords=False)
+    x1  = da.roll({lonname:n_roll},roll_coords=False)
+    x2  = da.roll({lonname:-1*n_roll},roll_coords=False)
     xgrad = (x1 - x2)/(2 * n_roll * xdist[None,:,None])
     #xgrad1 = (da.roll({'lon':n_roll}) - da.roll({'lon':-1*n_roll}))/(2 * n_roll * xdist[None,:,None])
     
@@ -106,12 +120,88 @@ def get_total_gradient(da, n_roll):
 n_roll  = 1
 sst     = xr.open_dataset(path+ncname)
 
+# sst = sst.rename_dims({'latitude' :'lat',
+#                   'longitude':'lon'})
+# sst = sst.rename_vars({'latitude' :'lat',
+#                   'longitude':'lon'})
+
 # Compute gradient
-gradsst = get_total_gradient(sst.sst,n_roll)
+gradsst = get_total_gradient(sst,n_roll,lonname='longitude',latname='latitude')
 
 #%% Get the latitude locations and the gulf stream
 
-lat_max,max_gradient,sst_grads = get_gs_coords_alltime(sst,n_roll,return_grad=True)
+lat_max,max_gradient,sst_grads = get_gs_coords_alltime(sst,n_roll,return_grad=True,
+                                                       lonname='longitude',latname='latitude',lonslice=[-75,-50])
+
+#%% Just visualize Gulf Stream Path (6-hourly)
+
+
+lonname='longitude'
+latname='latitude'
+
+ntime,nlat,nlon = sst_grads.shape
+im              = 0
+vlms            = [0,5]
+fig,ax = plt.subplots(1,1,subplot_kw={'projection':ccrs.PlateCarree()},figsize=(12,4),
+                       constrained_layout=True)
+
+
+
+ax.coastlines()
+ax.set_extent((-80,-50,20,55))
+
+plotsst = np.nanmean(sst_grads,0) * 100
+pcm     = ax.pcolormesh(sst_grads[lonname],sst_grads[latname],plotsst,vmin=vlms[0],vmax=vlms[1])
+
+for t in range(ntime):
+    
+    plot_index = sst_grads[latname].values[lat_max.values[t,:]]
+    
+    ax.scatter(lat_max[lonname],plot_index,s=5,alpha=0.05,transform=ccrs.PlateCarree(),color="k")
+    #plot_index = np.nanmean(max_gradient.reshape(int(ntime/12),12,nlon)[:,im,:],1)
+    #ax.plot(yrs,plot_index,label=im+1)
+ax.set_title("Month = %i" % (12))
+
+cb = fig.colorbar(pcm,ax=ax,orientation='horizontal',fraction=0.026,pad=0.01)
+cb.set_label("Mean Meridional Gradient of SST (K km$^{-1})$")
+
+ax.axvline([-68])
+
+figname = "%sMean_SST_ERSST_Gulf_StreamPath_6hourly.png" % figpath
+plt.savefig(figname,dpi=150)
+
+#%% Same as above but plot for each timestep
+
+
+vlms            = [0,5]
+
+for t in np.arange(0,ntime+1,4): # Plot everyday
+    fig,ax = plt.subplots(1,1,subplot_kw={'projection':ccrs.PlateCarree()},figsize=(12,4),
+                           constrained_layout=True)
+    
+    
+    
+    ax.coastlines()
+    ax.set_extent((-80,-50,20,55))
+    
+    plotsst = sst_grads.isel(time=t) * 100
+    pcm     = ax.pcolormesh(sst_grads[lonname],sst_grads[latname],plotsst,vmin=vlms[0],vmax=vlms[1])
+
+    plot_index = sst_grads[latname].values[lat_max.values[t,:]]
+    
+    ax.scatter(lat_max[lonname],plot_index,s=5,alpha=1,transform=ccrs.PlateCarree(),color="k")
+    #plot_index = np.nanmean(max_gradient.reshape(int(ntime/12),12,nlon)[:,im,:],1)
+    #ax.plot(yrs,plot_index,label=im+1)
+    ax.set_title("Time = %s" % (lat_max.time.isel(time=t).values))
+    
+    cb = fig.colorbar(pcm,ax=ax,orientation='horizontal',fraction=0.026,pad=0.01)
+    cb.set_label("Mean Meridional Gradient of SST (K km$^{-1})$")
+    
+    ax.axvline([-68])
+    
+    figname = "%sMean_SST_ERSST_Gulf_StreamPath_6hourly_%03i.png" % (figpath,t)
+    plt.savefig(figname,dpi=150,bbox_inches="tight")
+
 
 
 #%% Visualize Gulf Stream Index (Mean) by Month
@@ -168,7 +258,6 @@ plt.savefig(figname,dpi=150)
 
 #gradsst.sst.isel(time=0).plot()
 
-
 fig,ax = plt.subplots(1,1,subplot_kw={'projection':ccrs.PlateCarree()})
 vgrad_mean = gradsst.mean('time')
 im = ax.pcolormesh(vgrad_mean.lon,vgrad_mean.lat,vgrad_mean*100)
@@ -177,7 +266,6 @@ im = ax.pcolormesh(vgrad_mean.lon,vgrad_mean.lat,vgrad_mean*100)
 ax.set_extent((-80,-40,20,50))
 ax.coastlines()
 plt.colorbar(im,ax=ax)
-
 
 #%%
 
